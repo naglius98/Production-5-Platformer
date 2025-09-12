7790a335f095a3dc91be019654d1d0df94aa6e54
Gianmarco Nagliati Bravi <98173347+naglius98@users.noreply.github.com>
2025-09-11 21:16:37 -0400

    Added jumping
    - Player can now jump using w
    - Different jump heights can be reached depending on how long the jump button is held down

--- Production 5 platformer/Assets/Scripts/PlayerMovement.cs
+++ Production 5 platformer/Assets/Scripts/PlayerMovement.cs
@@ -4,16 +4,35 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
+
+    [Header("Movement")]
     public float MovementSpeed = 5.0f;
     private float HorizontalMovement;
+
+    [Header("Jumping")] 
+    public float JumpPower = 10.0f;
 
     void Update()
     {
         rb.linearVelocity = new Vector2(HorizontalMovement * MovementSpeed, rb.linearVelocity.y);
     }
+
+
     public void Move(InputAction.CallbackContext context)
     {
         HorizontalMovement = context.ReadValue<Vector2>().x;
 
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, JumpPower);
+        }
+        else if (context.canceled)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+        }
     }
 }
