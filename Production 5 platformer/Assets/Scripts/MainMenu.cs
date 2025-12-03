55026eaa149c140f9d8b048cc4f82d5506a554bd
Gianmarco Nagliati Bravi <98173347+naglius98@users.noreply.github.com>
2025-12-03 14:00:40 -0500

    Main menu
    - Added a main menu
    - Added main menu buttons functionalities

---
+++ Production 5 platformer/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void NewGame()
+    {
+        
+        SceneManager.LoadScene("GameScene");
+    }
+    
+    public void Instructions()
+    {
+        SceneManager.LoadScene("InstructionsScene");
+    }
+    
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+}
